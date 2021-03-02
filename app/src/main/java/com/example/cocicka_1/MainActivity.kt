9e951a91d2e64df3e349fa215341dfdce02b388a
Telepyzik1227 <ilya_timchenko@mail.ru>
2021-03-02 20:15:25 +0700

    5555

--- app/src/main/java/com/example/cocicka_1/MainActivity.kt
+++ app/src/main/java/com/example/cocicka_1/MainActivity.kt
@@ -2,10 +2,54 @@ package com.example.cocicka_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+cOne.setOnClickListener{ One_Result ("1",true)}
+        cTwo.setOnClickListener{ One_Result ("2",true)}
+        cThree.setOnClickListener{ One_Result ("3",true)}
+        cFour.setOnClickListener{ One_Result ("4",true)}
+        cFive.setOnClickListener{ One_Result ("5",true)}
+        cSix.setOnClickListener{ One_Result ("6",true)}
+        cSeven.setOnClickListener{ One_Result ("7",true)}
+        cEight.setOnClickListener{ One_Result ("8",true)}
+        cNine.setOnClickListener{ One_Result ("9",true)}
+        cZero.setOnClickListener{ One_Result ("0",true)}
+    cDot.setOnClickListener{ One_Result (".",true)}
+
+        cPlus.setOnClickListener{ One_Result (".",false)}
+        cMinus.setOnClickListener{ One_Result ("-",false)}
+        cUmnozhit.setOnClickListener{ One_Result ("+",false)}
+        cRazdelit.setOnClickListener{ One_Result ("/",false)}
+        cOpen.setOnClickListener{ One_Result ("(",false)}
+        cClose.setOnClickListener{ One_Result (")",false)}
+
+    cClear.setOnClickListener{
+        cResultat.text = ""
+        cReshenie.text = ""
+    }
+
+
+
     }
 }
+fun One_Result (var string: String,var Clear : Boolean) {
+    if(Clear){
+        cResultat.text = ""
+        cReshenie.append(string)
+
+    }else{
+        cReshenie.append(cResultat.text)
+        cReshenie.append(string)
+        cResultat.text = ""
+    }
+
+
+
+
+
+
+    }
