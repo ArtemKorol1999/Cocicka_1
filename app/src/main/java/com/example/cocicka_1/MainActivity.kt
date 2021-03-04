af1770c43cf73b5f01c073e99a9ee0213758ff65
Telepyzik1227 <ilya_timchenko@mail.ru>
2021-03-04 18:30:51 +0700

    ghzh;karjh:Ojt

--- app/src/main/java/com/example/cocicka_1/MainActivity.kt
+++ app/src/main/java/com/example/cocicka_1/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-cOne.setOnClickListener{ One_Result ("1",true)}
+        cOne.setOnClickListener{ One_Result ("1",true)}
         cTwo.setOnClickListener{ One_Result ("2",true)}
         cThree.setOnClickListener{ One_Result ("3",true)}
         cFour.setOnClickListener{ One_Result ("4",true)}
@@ -18,7 +18,7 @@ cOne.setOnClickListener{ One_Result ("1",true)}
         cEight.setOnClickListener{ One_Result ("8",true)}
         cNine.setOnClickListener{ One_Result ("9",true)}
         cZero.setOnClickListener{ One_Result ("0",true)}
-    cDot.setOnClickListener{ One_Result (".",true)}
+        cDot.setOnClickListener{ One_Result (".",true)}
 
         cPlus.setOnClickListener{ One_Result (".",false)}
         cMinus.setOnClickListener{ One_Result ("-",false)}
@@ -27,29 +27,31 @@ cOne.setOnClickListener{ One_Result ("1",true)}
         cOpen.setOnClickListener{ One_Result ("(",false)}
         cClose.setOnClickListener{ One_Result (")",false)}
 
-    cClear.setOnClickListener{
-        cResultat.text = ""
-        cReshenie.text = ""
+        cClear.setOnClickListener{
+            cResultat.text = ""
+            cReshenie.text = ""
+        }
+        cBS.setOnClickListener {
+            val string = cReshenie.text.toString()
+            if(string.isNotEmpty()){
+                cReshenie.text = string.substring(0,string.length-1)
+            }
+            cResultat.text = ""
+        }
+
+
     }
+    fun One_Result (string: String, Clear: Boolean) {
 
 
-
-    }
-}
-fun One_Result (var string: String,var Clear : Boolean) {
     if(Clear){
         cResultat.text = ""
         cReshenie.append(string)
-
-    }else{
+    }
+    else{
         cReshenie.append(cResultat.text)
         cReshenie.append(string)
         cResultat.text = ""
     }
-
-
-
-
-
-
-    }
+}
+}
